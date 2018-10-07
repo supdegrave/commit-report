a52b96d89a60688f6de9e9f0c9259374ceb27d21
Stuart Updegrave <stuart@updegrave.com>
2018-10-07 11:33:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# commit-report
+
+Node.js script which generates a report of Mercurial activity for the prior month. 
+
+## BUGS
+
+
+## TODO 
+
+* init repo and add to github
+* update hgOnClose to only generate reports / send email once all projects have been queried
+* add instructions in README for sending email
+* Move ChangeSet class into separate file
+* set up bash script to run cron job; see the following: https://medium.com/@gattermeier/cronjobs-for-your-node-js-apps-on-macos-20d129b42c0e
+* update to run for current month (default to previous month)
+* include day of week short name in daily report details
+* include project name for each changeset
+* sort daily changesets by commit time (add to ChangeSet)
+
+## DONE
+
+* pass arguments into node script, parse with https://www.npmjs.com/package/minimist
+* extract username into bash script
+* extract gmail PII into bash script
+* extract projects into bash script
+* If only a single project is passed, wrap in array
+* replace `path` with `projects`, an array of objects: { name, path }, update main to forEach over projects, initializing hgLog and assigning event handlers for each project
---
+++ get-checkin-days.js
@@ -0,0 +1,175 @@
+// ============================
+// get-checkin-days.js
+// ============================
+// Examines Mercurial history for a given month / user combination, and determines the total number of days containing checkins. 
+
+const util = require('util');
+const exec$ = util.promisify(require('child_process').exec);
+
+const shortMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const today = new Date();
+const lastMonthLastDay = new Date(today.getFullYear(), today.getMonth(), 0);
+const lastMonthShortName = shortMonthNames[lastMonthLastDay.getMonth()];
+
+// parse command-line arguments
+const arguments = require('minimist')(process.argv.slice(2));
+const projects = parseProjects(arguments);
+
+const args = [
+    'hg',
+    'log',
+    '--user',
+    arguments.user,
+    '--date',
+    `'${lastMonthShortName} ${lastMonthLastDay.getFullYear()}'`,
+    '--no-merges'
+];
+
+let changeSets = [];
+// let allCommits = [];
+let reportText = [];
+let reportSubject = '';
+
+function main() {
+    const projectCommitsArray$ = projects.map(getProjectCommits);
+
+    Promise.all(projectCommitsArray$)
+        .then(projectCommits$ => {
+            projectCommits$.forEach(projectCommits => {
+                const { project, commits } = projectCommits;    
+                const projectChangeSets = commits.map(commit => new ChangeSet(project, commit));
+                changeSets = changeSets.concat(projectChangeSets);
+            });
+        })
+        .then(() => {
+            generateCommitReport();
+            console.log(reportText.join(''));
+        })
+        .catch(console.error);
+}
+
+async function getProjectCommits(project) {
+    const cmd = args.join(' ');
+    const options = { cwd: project.path };
+
+    try {
+        return await exec$(cmd, options)
+            .then(response => { 
+                const commits = response.stdout.toString().trim().split(/\n\n/);
+                return { project, commits };
+            })
+    } catch(err) {
+        console.error(`error: $err`);
+    }
+}
+
+function parseProjects(arguments) {
+    let projects = [];
+
+    if (arguments.project) {
+        if (!(arguments.project instanceof Array)) {
+            console.log(arguments.project);
+            arguments.project = [arguments.project];
+            console.log(arguments.project);
+        }
+        projects = arguments.project.map(p => {
+            const projectParts = p.split(':');
+            return {
+                name: projectParts[0],
+                path: projectParts[1]
+            };
+        });
+    }
+
+    return projects;
+}
+
+function generateCommitReport() {
+    const dateSortedChangeSets = changeSets
+        .sort((cs1, cs2) => cs1.date - cs2.date);
+
+    const datesWithChangeSets = dateSortedChangeSets
+        .map(cs => cs.dayOfMonth)
+        .filter((val, inx, arr) => arr.indexOf(val) === inx);
+
+    const changeSetsByDate = datesWithChangeSets
+        .map(date => {
+            const changeSets = dateSortedChangeSets.filter(cs => cs.dayOfMonth === date);
+            return { date, changeSets };
+        });
+
+    reportSubject = `${lastMonthShortName} ${lastMonthLastDay.getFullYear()}: ${datesWithChangeSets.length} days with commits`;
+
+    addReportText(`-------------------------------------------`);
+    addReportText(reportSubject);
+    addReportText(`-------------------------------------------`);
+    addReportText(``);
+
+    changeSetsByDate.forEach(csbd => {
+        addReportText(`${lastMonthShortName} ${csbd.date}: ${csbd.changeSets.length} commit(s)`);
+        addReportText(`-------------------------------------------`);
+        csbd.changeSets.forEach(cs => addReportText(`${cs.projectName}: ${cs.changeset} \n => ${cs.summary}`));
+        addReportText(`-------------------------------------------`);
+        addReportText(``);
+    });
+}
+
+function addReportText(string) {
+    reportText.push(`${string} \n`);
+}
+
+function hgOnStdout(data) {
+    const commits = data.toString().trim().split(/\n\n/);
+    allCommits = allCommits.concat(commits);
+}
+
+function hgOnStderror(data) {
+    console.error(`hg log error => ${data.toString()}`);
+}
+
+function hgOnClose(code) {
+    if (!code) {
+        generateCommitReport(allCommits);
+        console.log(reportText.join(''));
+    } else { // non-zero indicates that an error occurred
+        console.log(`child process exited with code ${code}`);
+    }
+};
+
+main();
+
+
+// ##### supporting classes ##################################
+
+// TODO: move to separate file, import
+// class ChangeSet {
+//     constructor(rawChangeset) {
+//         const re = /changeset\:\s+([^$]+)$\n^user\:\s+([^$]+)$\n^date\:\s+([^$]+)$\n^summary\:\s+([^$]+)/m;
+//         const matches = rawChangeset.match(re);
+
+//         if (matches) {
+//             this.changeset = matches[1];
+//             this.date = new Date(matches[3]);
+//             this.summary = matches[4];
+
+//             this.dayOfMonth = this.date.getDate();
+//         }
+//     }
+// }
+
+class ChangeSet {
+    constructor(project, commitText) {
+        const re = /changeset\:\s+([^$]+)$\n^user\:\s+([^$]+)$\n^date\:\s+([^$]+)$\n^summary\:\s+([^$]+)/m;
+        const matches = commitText.match(re);
+
+        if (matches) {
+            this.projectName = project.name;
+            this.changeset = matches[1];
+            this.date = new Date(matches[3]);
+            this.summary = matches[4];
+
+            this.dayOfMonth = this.date.getDate();
+        }
+    }
+}
+
